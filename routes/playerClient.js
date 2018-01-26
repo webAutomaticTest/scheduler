2e5f405c2715ec9b40da306d4b57cb698121f034
chenweiLabri <888chenwei666@gmail.com>
2018-01-26 13:44:03 +0100

    play scenario feedback change

--- routes/playerClient.js
+++ routes/playerClient.js
@@ -2,6 +2,7 @@ const winston = require('winston');
 const amqp = require('amqplib');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require('mongodb').ObjectID;
+const Crontab = require('crontab');
 
 const QUEUE_NAME = 'player_queue';
 
@@ -14,7 +15,14 @@ module.exports.init = function(serverNames, webServer) {
 	.get('/playNow/:sid', async (req, res) => {
 		await playNow(req,res, req.params.sid);
 	})
-	
+	.get('/playSchedule/:sid', (req, res) => {
+		const options = {
+			cron : '0 6 * * *',
+			sid : req.params.sid
+		};
+		addCron(req,res, options);
+	})
+
 	function generateUuid() {
 		console.log("math random");
 		return Math.random().toString() +
@@ -57,7 +65,7 @@ module.exports.init = function(serverNames, webServer) {
 										setTimeout(function() { 
 											channel.close();
 											db.close();
-											res.status(200).send(`play request sent for scenario ${sid} is finish? :${msgFeedback.content.toString()}`).end(); 
+											res.status(200).send(msgFeedback.content.toString()).end(); 
 										}, 500);
 									}
 								}, {noAck: true});
@@ -84,6 +92,32 @@ module.exports.init = function(serverNames, webServer) {
 			winston.info(err);
 			res.send(err).status(500).end;
 		});
+	}
+
+	function addCron(req, res, playOptions) {
+		const cron = playOptions.cron;
+		const comment = playOptions.sid;
+		winston.info(`Add new cron for scenario (${playOptions.sid}) with cron ${cron}`);
+		if (cron) {
+			Crontab.load((err, ct) => {
+				const NODE_BIN = '/usr/local/bin/node';
+				const NODE_SCRIPT = '/tmp/scheduler/sendPlayMessage.js';
+				const NODE_OPTIONS = `--mongo=mongo --rabbit=rabbit --sid=${playOptions.sid}`;
+				const LOG = '>> /var/log/watcron.log 2>&1';
+				ct.create(`${NODE_BIN} ${NODE_SCRIPT} ${NODE_OPTIONS} ${LOG}`, cron, comment);
+				
+				ct.save((err) => {
+					if (err) {
+						res.status(500).send(err).end();
+					}
+					else {
+						res.status(200).send(`cron request sent for scenario ${playOptions.sid}`).end();
+					}
+				}); 
+			});
+		} else {
+			res.status(500).send(`cron ${cron} is not valid`).end();
+		}
 	}
 
 };
